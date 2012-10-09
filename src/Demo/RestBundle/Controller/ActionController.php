3b3620bc9b1cf3b1b54079b1f798e13c9e911a03
playtime <playtime@localhost>
2012-10-09 02:17:56 +0200

    End of REST CRUD

--- src/Demo/RestBundle/Controller/ActionController.php
+++ src/Demo/RestBundle/Controller/ActionController.php
@@ -4,60 +4,105 @@ namespace Demo\RestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Demo\RestBundle\Entity\Customer;
-use Demo\RestBundle\Entity\CustomerRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class ActionController extends Controller
 {
 
-	public function getOneAction($id)
-	{
-		$customers = array();
-		$customer = $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->find($id);
-		if ($customer === null) {
-			throw $this->createNotFoundException('Customer[id=' . $id . '] inexistant.');
-		}
-		$customers[] = $customer;
-		return $this->render('DemoRestBundle:Rest:show.' . $this->getTemplateName($this->getRequest()->getAcceptableContentTypes()) . '.twig', array('customers' => $customers));
-	}
+    public function getOneAction($id)
+    {
+        $customers = array();
+        $customer = $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->find($id);
+        if ($customer === null) {
+            throw $this->createNotFoundException('Can\'t find customer with id : ' . $id . '.');
+        }
+        $customers[] = $customer;
 
-	public function getAllAction()
-	{
-		$customers = $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->findAll();
-		if (count($customers) == 0) {
-			$this->addBuddies();
-			$customers = $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->findAll();
-		}
-		return $this->render('DemoRestBundle:Rest:show.' . $this->getTemplateName($this->getRequest()->getAcceptableContentTypes()) . '.twig', array('customers' => $customers));
-	}
+        $acceptableContentTypes = $this->getRequest()->getAcceptableContentTypes();
 
-	protected function addBuddies()
-	{
-		$customer = new Customer();
-		$customer->setFirstName('Boby');
-		$customer->setLastName('Jacks');
-		$customer->setEmailAddress('bjacks@plop.com');
-		$this->getDoctrine()->getEntityManager()->persist($customer);
+        $response = new Response();
+        $response->setContent($this->renderView('DemoRestBundle:Rest:show.' . $this->getTemplateName($acceptableContentTypes[0]) . '.twig', array('customers' => $customers)));
+        $response->headers->set('Content-Type', $acceptableContentTypes[0]);
 
-		$customer = new Customer();
-		$customer->setFirstName('John');
-		$customer->setLastName('Doe');
-		$customer->setEmailAddress('jdoe@plop.com');
-		$this->getDoctrine()->getEntityManager()->persist($customer);
-		
-		$this->getDoctrine()->getEntityManager()->flush();
-	}
+        return $response;
+    }
 
-	protected function getTemplateName($acceptableContentTypes)
-	{
-		foreach ($acceptableContentTypes as $contentType) {
-			if ($contentType == 'application/xml') {
-				return 'xml';
-			} elseif ($contentType == 'application/json') {
-				return 'json';
-			} elseif ($contentType == 'text/html') {
-				return 'html';
-			}
-		}
-		throw $this->createNotFoundException('Bad content type requested');
-	}
+    public function getAllAction()
+    {
+        $customers = $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->findAll();
+        if (count($customers) == 0) {
+            $this->addBuddies();
+            $customers = $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->findAll();
+        }
+
+        $acceptableContentTypes = $this->getRequest()->getAcceptableContentTypes();
+
+        $response = new Response();
+        $response->setContent($this->renderView('DemoRestBundle:Rest:show.' . $this->getTemplateName($acceptableContentTypes[0]) . '.twig', array('customers' => $customers)));
+        $response->headers->set('Content-Type', $acceptableContentTypes[0]);
+
+        return $response;
+    }
+
+    public function newAction()
+    {
+        try {
+            $customer = $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->addCustomer($this->getRequest()->request->all());
+            return $this->redirect($this->generateUrl('rest_get_one', array('id' => $customer->getId())));
+        } catch (\Exception $exc) {
+            throw $this->createNotFoundException($exc->getMessage());
+        }
+    }
+
+    public function editAction()
+    {
+        try {
+            $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->editCustomer($this->getRequest()->request->all());
+            return $this->redirect($this->generateUrl('rest_get_all'));
+        } catch (\Exception $exc) {
+            throw $this->createNotFoundException($exc->getMessage());
+        }
+    }
+
+    public function deleteAction()
+    {
+        $customer = $this->getDoctrine()->getEntityManager()->getRepository('DemoRestBundle:Customer')->find($this->getRequest()->request->get('id'));
+        if ($customer === null) {
+            throw $this->createNotFoundException('Can\'t find customer with id : ' . $this->getRequest()->request->get('id') . '.');
+        }
+        $this->getDoctrine()->getEntityManager()->remove($customer);
+        $this->getDoctrine()->getEntityManager()->flush();
+        $response = new Response();
+        return $response;
+    }
+
+    protected function addBuddies()
+    {
+        $customer = new Customer();
+        $customer->setFirstName('Boby');
+        $customer->setLastName('Jacks');
+        $customer->setEmailAddress('bjacks@plop.com');
+        $this->getDoctrine()->getEntityManager()->persist($customer);
+
+        $customer = new Customer();
+        $customer->setFirstName('John');
+        $customer->setLastName('Doe');
+        $customer->setEmailAddress('jdoe@plop.com');
+        $this->getDoctrine()->getEntityManager()->persist($customer);
+
+        $this->getDoctrine()->getEntityManager()->flush();
+    }
+
+    protected function getTemplateName($contentType)
+    {
+        if ($contentType == 'application/xml') {
+            return 'xml';
+        } elseif ($contentType == 'application/json') {
+            return 'json';
+        } elseif ($contentType == 'text/html') {
+            return 'html';
+        }
+        throw $this->createNotFoundException('Bad content type requested');
+    }
+
 }
--- src/Demo/RestBundle/Entity/CustomerRepository.php
+++ src/Demo/RestBundle/Entity/CustomerRepository.php
@@ -12,4 +12,75 @@ use Doctrine\ORM\EntityRepository;
  */
 class CustomerRepository extends EntityRepository
 {
+    
+    /**
+     *  Add a new customer
+     * 
+     * @param array $postParams
+     * @return \Demo\RestBundle\Entity\Customer
+     * @throws \Exception
+     */
+    public function addCustomer($postParams)
+    {
+        if (is_array($postParams) === false) {
+            throw new \Exception('Missing parameter');
+        }
+        if (empty($postParams['first_name']) === true) {
+            throw new \Exception('Missing parameter "first_name"');
+        }
+        if (empty($postParams['last_name']) === true) {
+            throw new \Exception('Missing parameter "last_name"');
+        }
+        if (empty($postParams['email_address']) === true) {
+            throw new \Exception('Missing parameter "email_address"');
+        }
+        $customer = new Customer();
+        $customer->setFirstName($postParams['first_name']);
+        $customer->setLastName($postParams['last_name']);
+        $customer->setEmailAddress($postParams['email_address']);
+        $this->getEntityManager()->persist($customer);
+        $this->getEntityManager()->flush();
+        
+        return $customer;
+    }
+
+    /**
+     *  Edit an existing customer
+     * 
+     * @param array $postParams
+     * @return boolean
+     * @throws \Exception
+     */
+    public function editCustomer($postParams)
+    {
+        if (is_array($postParams) === false) {
+            throw new \Exception('Missing parameter');
+        }
+        if (empty($postParams['id']) === true) {
+            throw new \Exception('Missing parameter "id"');
+        }
+        if (empty($postParams['first_name']) === true) {
+            throw new \Exception('Missing parameter "first_name"');
+        }
+        if (empty($postParams['last_name']) === true) {
+            throw new \Exception('Missing parameter "last_name"');
+        }
+        if (empty($postParams['email_address']) === true) {
+            throw new \Exception('Missing parameter "email_address"');
+        }
+        
+        $customer = $this->find($postParams['id']);
+        if ($customer == null) {
+            throw new \Exception('Can\'t find customer with id : ' . $postParams['id'] . '.');
+        }
+        
+        $customer->setFirstName($postParams['first_name']);
+        $customer->setLastName($postParams['last_name']);
+        $customer->setEmailAddress($postParams['email_address']);
+        $this->getEntityManager()->persist($customer);
+        $this->getEntityManager()->flush();
+        
+        return true;
+    }
+    
 }
